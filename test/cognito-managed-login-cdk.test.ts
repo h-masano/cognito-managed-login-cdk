603300a6aee95f86ea64ee1a2ba9184f201de00b
h-masano <hiroki.masanon@gmail.com>
2024-12-28 22:15:01 +0900

    Initial commit

---
+++ test/cognito-managed-login-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CognitoManagedLoginCdk from '../lib/cognito-managed-login-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cognito-managed-login-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CognitoManagedLoginCdk.CognitoManagedLoginCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
